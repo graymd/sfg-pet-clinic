af5f377ecf7133d05c1ea5bdf36e73d2a5a8e78a
Malachi Gray <graymd.cfi@gmail.com>
2018-08-13 11:56:49 -0400

    adding POJOs.  Closes #2

---
+++ src/main/java/com/malachigray/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.malachigray.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/malachigray/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.malachigray.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
