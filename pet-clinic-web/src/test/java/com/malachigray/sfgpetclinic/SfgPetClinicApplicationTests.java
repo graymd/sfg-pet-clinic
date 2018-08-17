354a892aa7c021be6bb13a6b94f9aa0cd40ef5c4
Malachi Gray <graymd.cfi@gmail.com>
2018-08-16 23:08:29 -0400

    add maven-release-plugin

--- pet-clinic-web/src/test/java/com/malachigray/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/malachigray/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,12 +1,13 @@
-package sfgpetclinic;
+package com.malachigray.sfgpetclinic;
 
+import com.malachigray.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SfgPetClinicApplication.class)
 public class SfgPetClinicApplicationTests {
 
     @Test
