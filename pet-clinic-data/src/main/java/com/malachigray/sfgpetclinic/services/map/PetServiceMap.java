c655e413468a0761e587c7509d90a1fccbd99d12
Malachi Gray <graymd.cfi@gmail.com>
2018-08-18 15:13:07 -0400

    implement Map based services, closes #6

---
+++ pet-clinic-data/src/main/java/com/malachigray/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package com.malachigray.sfgpetclinic.services.map;
+
+import com.malachigray.sfgpetclinic.model.Owner;
+import com.malachigray.sfgpetclinic.model.Pet;
+import com.malachigray.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
